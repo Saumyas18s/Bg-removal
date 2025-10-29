c4e3813132a477679f217ba87e77cbd7580b42dd
Saumya Singh <saumyasingh62552@gmail.comm>
2025-10-29 20:22:13 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 import connectDB from './configs/mongodb.js';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-await connectDB();
-app.use(express.json());
-app.use(cors());
-app.get('/',(req, res)=>{ res.send("API working")});
 
-app.listen(PORT, ()=> console.log(
-    `Server running on port ${PORT}`
-))
+(async () => {
+  try {
+    await connectDB();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.get('/', (req, res) => {
+      res.send("API working");
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+})();
